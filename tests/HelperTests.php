fe42e470fb473ab897b00c4b568aa705658d5e18
Aaron Schmied <aaron@pro-sales.ch>
2018-03-26 15:51:54 +0200

    Update the tests for the helper.

--- tests/HelperTests.php
+++ tests/HelperTests.php
@@ -2,12 +2,35 @@
 
 namespace RichanFongdasen\EloquentBlameableTest;
 
+use RichanFongdasen\EloquentBlameableTest\Models\Post;
+use RichanFongdasen\EloquentBlameableTest\Models\PostWithoutAttributes;
+
+use RichanFongdasen\EloquentBlameable\BlameableService;
+
 class HelperTests extends TestCase
 {
+
+    /**
+     * The Eloquent model to test the helper function on
+     *
+     * @var Post
+     */
+    protected $post;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->post = factory(Post::class)->make();
+    }
+
     /** @test */
     public function it_returns_current_user_identifier_correctly1()
     {
-        $this->assertNull(blameable_user());
+        $this->assertNull(blameable_user($this->post));
     }
 
     /** @test */
@@ -15,7 +38,7 @@ class HelperTests extends TestCase
     {
         $this->impersonateUser();
 
-        $this->assertEquals($this->user->getKey(), blameable_user());
+        $this->assertEquals($this->user->getKey(), blameable_user($this->post));
     }
 
     /** @test */
@@ -23,6 +46,6 @@ class HelperTests extends TestCase
     {
         $this->impersonateOtherUser();
 
-        $this->assertEquals($this->otherUser->getKey(), blameable_user());
+        $this->assertEquals($this->otherUser->getKey(), blameable_user($this->post));
     }
 }
