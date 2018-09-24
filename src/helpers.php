1aff5b5283f5ee62a4008414ed1bb30ab0775d8f
Fredy Mendivelso <fredy.mendivelso@placetopay.com>
2018-09-24 10:50:59 -0500

    Not trow exception/error when user is not authenticatede

--- src/helpers.php
+++ src/helpers.php
@@ -7,16 +7,19 @@ if (!function_exists('blameable_user')) {
     /**
      * Get the blameable User identifier.
      *
+     * @param Model $model
+     * 
      * @return mixed
      */
     function blameable_user(Model $model)
     {
         $guard = app(BlameableService::class)->getConfiguration($model, 'guard');
 
+        /** @var Model $user */
         $user = ($guard === null) ? \Auth::user() : \Auth::guard($guard)->user();
         $userClass = app(BlameableService::class)->getConfiguration($model, 'user');
 
-        if ($user instanceof $userClass) {
+        if ($user && $user instanceof $userClass) {
             return $user->getKey();
         }
 
