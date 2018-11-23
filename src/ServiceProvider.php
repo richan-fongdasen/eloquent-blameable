e8bea96321b57bc85d662f10cf6eaccf8dd27827
Andy Pfister <andy.pfister@hotmail.ch>
2018-11-23 11:49:13 +0100

    Ignore publishing of config file if package is run under Lumen

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,15 +7,27 @@ use Illuminate\Support\ServiceProvider as Provider;
 class ServiceProvider extends Provider
 {
     /**
+     * Check if the application runs with Lumen.
+     *
+     * @return bool
+     */
+    protected function isLumen()
+    {
+        return strpos($this->app->version(), 'Lumen') !== false;
+    }
+
+    /**
      * Bootstrap the application services.
      *
      * @return void
      */
     public function boot()
     {
-        $this->publishes([
-            realpath(__DIR__.'/../config/blameable.php') => config_path('blameable.php'),
-        ], 'config');
+        if (!$this->isLumen()) {
+            $this->publishes([
+                realpath(__DIR__ . '/../config/blameable.php') => config_path('blameable.php'),
+            ], 'config');
+        }
     }
 
     /**
@@ -25,7 +37,7 @@ class ServiceProvider extends Provider
      */
     public function register()
     {
-        $this->mergeConfigFrom(realpath(__DIR__.'/../config/blameable.php'), 'blameable');
+        $this->mergeConfigFrom(realpath(__DIR__ . '/../config/blameable.php'), 'blameable');
 
         $this->app->singleton(BlameableObserver::class, function () {
             return new BlameableObserver();
