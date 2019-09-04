0cbed6788dd4b27a816439467d1b84ca6ca1f15f
Richan Fongdasen <richan-fongdasen@users.noreply.github.com>
2019-09-04 22:44:16 +0700

    Add support for Laravel 6.0 (#21)
    * Add support for Laravel 6.0 and drop some of the older versions except 5.8
    
    * Update travis configuration

--- tests/Supports/database/factories/UserFactory.php
+++ tests/Supports/database/factories/UserFactory.php
@@ -6,7 +6,7 @@ $factory->define(User::class, static function (\Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(12)),
-        'remember_token' => str_random(12),
+        'password' => bcrypt(Str::random(12)),
+        'remember_token' => Str::random(12),
     ];
 });
